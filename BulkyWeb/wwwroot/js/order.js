fe7f48ccf60a548370c06f59e2a022dfa00028f4
Kamal Kadayat <kmlkadayat@gmail.com>
2025-01-12 10:29:08 +0545

    Order List generated for admin

---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    if ($.fn.DataTable.isDataTable('#tblData')) {
+        $('#tblData').DataTable().destroy();
+    }
+
+    $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/order/getall',
+            dataSrc: 'data' 
+        },
+        "columns": [
+            { data: 'id' },
+            { data: 'name' },
+            { data: 'phoneNumber' },
+            {
+                data: null,
+                render: function (data, type, row) {
+                    return data.applicationUser ? data.applicationUser.email : 'N/A';
+                }
+            },
+            { data: 'orderStatus' },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `
+                        <div class="btn-group" role="group">
+                            <a href="/admin/order/orderId?id=${data}" class="btn btn-primary">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                        </div>`;
+                }
+            }
+        ],
+        "order": [[0, "asc"]],
+        "paging": true,
+        "searching": true,
+        "info": true
+    });
+});
