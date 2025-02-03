2541d53f42e2330feae6043fef03ff56f0b405ac
Kamal Kadayat <kmlkadayat@gmail.com>
2025-02-03 16:59:16 +0545

    style and blog model added

---
+++ Bulky.Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? BlogImage { get; set; }
+        [NotMapped]
+        public IFormFile? BlogImageFile { get; set; }
+    }
+}
