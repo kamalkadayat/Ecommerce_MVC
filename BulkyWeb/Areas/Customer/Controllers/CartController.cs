1e4bef2d4213ba0a465ef96cc56e05f6e8c41cb3
Kamal Kadayat <kmlkadayat@gmail.com>
2025-02-15 20:47:17 +0545

    UserManagement Completed

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -120,7 +120,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
             {
                 //it is a regular account and we need to capture payment
                 //stripe logic
-                var domain = "https://localhost:7164/";
+                var domain = "https://localhost:44303/";
                 var options = new Stripe.Checkout.SessionCreateOptions
                 {
                     SuccessUrl = domain+ $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
